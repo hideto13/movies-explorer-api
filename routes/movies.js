c6513c3cda521ca299e7b3609631be61f297bfa1
Tanya <pinki_00@mail.ru>
2022-04-23 17:14:03 +0300

    Add movies routes and controllers

---
+++ routes/movies.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const {
+  getMovies,
+  createMovie,
+  deleteMovie,
+} = require('../controllers/movies');
+
+router.get('/', getMovies);
+
+router.delete('/:movieId', celebrate({
+  params: Joi.object().keys({
+    movieId: Joi.number().required(),
+  }),
+}), deleteMovie);
+
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().pattern(/https*:\/\/(www.)?([A-Za-z0-9]{1}[A-Za-z0-9-]*\.?)*\.{1}[A-Za-z0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/),
+    trailerLink: Joi.string().required().pattern(/https*:\/\/(www.)?([A-Za-z0-9]{1}[A-Za-z0-9-]*\.?)*\.{1}[A-Za-z0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+    thumbnail: Joi.string().required().pattern(/https*:\/\/(www.)?([A-Za-z0-9]{1}[A-Za-z0-9-]*\.?)*\.{1}[A-Za-z0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/),
+    movieId: Joi.number().required(),
+  }),
+}), createMovie);
+
+module.exports = router;
